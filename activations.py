6ffce27ed9568eb5c8ba85b0c7485c64ba8d64bc
James Oldfield <hi@james-oldfield.co>
2017-09-04 11:13:24 +0100

    add sigmoid activation functions for output layer

--- activations.py
+++ activations.py
@@ -9,7 +9,7 @@ def relu(z):
     """Compute the rectified linear activation of unit z.
     N.B. Freshly allocates a new array each function called.
 
-    :param x: The array of values for a unit, pre-activation.
+    :param z: The array of values for a unit, pre-activation.
     :return: ReLu evaluated at z.
     """
 
@@ -27,3 +27,22 @@ def d_relu(x):
         return 1
     else:
         return 0
+
+
+def sigmoid(z):
+    """Compute the sigmoid activation of z
+
+    :param z: The array of values for a unit, pre-activation.
+    :return: the sigmoid activation of z
+    """
+
+    return 1.0 / (1.0 + np.exp(-z))
+
+
+def d_sigmoid(x):
+    """Compute the deriv. of sigmoid w/r/t x
+
+    :param x: scalar value we wish to evaluate the gradient at.
+    :return: d_sigmoid/d x
+    """
+    return sigmoid(x) * (1 - sigmoid(x))
