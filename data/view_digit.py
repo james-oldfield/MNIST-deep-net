194695b8269908df7eb9b6e269a9eec6430b57fc
James Oldfield <hi@james-oldfield.co>
2017-09-04 16:32:37 +0100

    print ground truth digit val

--- data/view_digit.py
+++ data/view_digit.py
@@ -18,5 +18,7 @@ else:
 # rid of rank 1 array format
 random_digit = training_set[0][index].reshape(28, -1)
 
+print('this digit is a {}'.format(training_set[1][index]))
+
 plt.imshow(random_digit)
 plt.show()
