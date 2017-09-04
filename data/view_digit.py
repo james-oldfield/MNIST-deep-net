be13d61856993c0cb8bc127e96260d36df06bc6f
James Oldfield <hi@james-oldfield.co>
2017-09-04 16:25:48 +0100

    move utils to data folder + add viewing script

---
+++ data/view_digit.py
@@ -0,0 +1,22 @@
+"""
+View a random digit from MNIST dataset, or supply an index in CLI.
+"""
+import matplotlib.pyplot as plt
+
+from sys import argv
+from random import randint
+from data import get_MNIST
+
+training_set, _, _ = get_MNIST()
+
+# optional CLI index
+if len(argv) < 2:
+    index = randint(0, len(training_set[1]))
+else:
+    index = argv[1]
+
+# rid of rank 1 array format
+random_digit = training_set[0][index].reshape(28, -1)
+
+plt.imshow(random_digit)
+plt.show()
