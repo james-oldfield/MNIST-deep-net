78979a167f17b54e43522a8bef6915bbc8ee3503
James Oldfield <hi@james-oldfield.co>
2017-09-05 11:35:39 +0100

    pass test set into GD method

--- train.py
+++ train.py
@@ -1,8 +1,7 @@
 from data import data
 import deep_network
 
-training_data, _, _ = data.load_data_wrapper()
+training_data, _, test_data = data.load_data_wrapper()
 
-nn = deep_network.DeepNet([784, 30, 10])
-
-nn.SGD(training_data, 30, 10, 3.0, debug=True)
+nn = deep_network.DeepNet([784, 30, 30, 10])
+nn.SGD(training_data, 30, 10, 1.0, test_data=test_data)
