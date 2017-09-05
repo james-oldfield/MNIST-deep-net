d45f2a9079993f3a6fa1b31c8fb0406767f715cd
James Oldfield <hi@james-oldfield.co>
2017-09-05 09:20:09 +0100

    add functions to one-hot encode y values

--- data/data.py
+++ data/data.py
@@ -1,5 +1,6 @@
 import pickle
 import gzip
+import numpy as np
 
 
 def get_MNIST():
@@ -12,3 +13,18 @@ def get_MNIST():
     f.close()
 
     return (training_data, validation_data, test_data)
+
+
+def load_data_wrapper():
+    tr_d, va_d, te_d = get_MNIST()
+    training_results = [one_hot(y) for y in tr_d[1]]
+    training_data = (tr_d[0], training_results)
+
+    return (training_data, va_d, te_d)
+
+
+def one_hot(j):
+    e = np.zeros((10, 1))
+    e[j] = 1.0
+
+    return e
